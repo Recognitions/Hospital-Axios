5a21eb72a0ef50206b64f53ff53726cf18420832
GeronimoNeto <102226234+GeronimoNeto@users.noreply.github.com>
2022-09-09 16:31:43 -0300

    event

--- app/Http/Controllers/Eventos.php
+++ app/Http/Controllers/Eventos.php
@@ -12,38 +12,28 @@ class Eventos extends Controller
 {
     public function index(){
         $pacientes = Pacientes::all();
-        
         return redirect('/painel');
     }
-
     public function pacientes(){
         $pacientes = Pacientes::all();
-
         return view('pacientes',
             [
                 "pacientes" => $pacientes
             ]
         );
     }
-
     public function renderPatients(){
         $pacientes = Pacientes::all();
-
         return $pacientes;
     }
-
     public function salvar(Request $r){
-        //return redirect('/painel/pacientes/novo');
-        
         $event = new Pacientes;
         $cpf = $r->cpf;
-
         $event->nome = $r->nome;
         $event->cpf = $cpf;
         $event->wpp = $r->wpp;
         $event->nasc = $r->nasc;
         $event->estado = 3;
-        
         if($r->hasFile("foto") && $r->file("foto")->isValid()){
             $permitidas = ["jpg","png","jfif"];
             $foto = $r->foto;
@@ -56,24 +46,19 @@ class Eventos extends Controller
                 return redirect('/painel/pacientes')->with('alert2','Formato de Imagem Não Permitido!');
             }
         }
-        
         $event->save();
-
         $select = $event->select('id','nome','nasc','cpf','wpp','foto','estado')->where('cpf',$cpf)->get();
         return response()->json($select);
     }
     public function editar(Request $r){
         $event = new Pacientes;
-
         $cpf = $r->cpf;
         $wpp = $r->wpp;
-
         $id = $r->id;
         $event->nome = $r->nome;
         $event->cpf = $cpf;
         $event->wpp = $wpp;
         $event->nasc = $r->nasc;
-
         if($r->hasFile("foto") && $r->file("foto")->isValid()){
             $permitidas = ["jpg","jpeg","png","jfif"];
             $foto = $r->foto;
@@ -85,7 +70,6 @@ class Eventos extends Controller
                 return redirect('/painel/pacientes')->with('alert2','Formato de Imagem Não Permitido!');
             }
         }else{$nomeFoto="none.jpg";}
-        
         $select = $event->where('id',$id)->update([
             "nome"=>$r->nome,
             "cpf"=>$cpf,
@@ -93,48 +77,30 @@ class Eventos extends Controller
             "nasc"=>$r->nasc,
             "foto"=>$nomeFoto
         ]);
-
-        //return response()->json(['Paciente Editado']);
         return response()->json($select);
     }
-
     public function atender($id){
-        //echo $id;
-
         $event = Pacientes::findOrFail($id);
         return view('atender',["event" => $event]);
     }
     public function concluir(Request $r){
-        //echo $id;
         $event = new Atendimentos;
-
         $event->sintomas = $r->sintomas;
         $event->lista = "$r->lista";
         $event->resultado = $r->resultado;
         $event->idPaciente = $r->idPaciente;
-
         $paci = new Pacientes;
-
-
         if($event->where('idPaciente', $event->idPaciente)->count() == 0) {
             $event->save();
         }else{
             $event->where('idPaciente',$event->idPaciente)->update($r->all());
         }
         $paci->where('id',$event->idPaciente)->update(['estado'=>$event->resultado]);
-
         return response()->json(['Paciente Atendido']);
-        //return redirect("/painel/pacientes/atender/$event->idPaciente")->with('alert3','Paciente Atendido!');
     }
-
     public function remover($id){
-
         $event = new Pacientes;
-
         $event->where('id',$id)->firstorfail()->delete();
-
-        //return response()->json(['Paciente Deletado']);
         return redirect('/painel/pacientes')->with('alert4','Paciente Removido!');
     }
-
 }
